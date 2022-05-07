a1d06333d05324482a1f7f8916bfcc073e33e03b
Eurico Da Custodia <rico.alves91@gmail.com>
2022-05-07 18:54:06 +0200

    added model user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
+  validates :email, presence: true,  format: {
+    with: EMAIL_REGEX, 
+    message: 'invalid email address provided'
+  }
+end
